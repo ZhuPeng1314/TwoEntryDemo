0b1b211c3a048db664ba29c9fb4c1596dd8cc45f
ZhuPeng1314 <mo.shang1@qq.com>
2015-12-14 20:21:02 +0800

    创建项目

---
+++ ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  TwoEntryDemo
+//
+//  Created by 鹏 朱 on 15/12/14.
+//  Copyright © 2015年 鹏 朱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        self.view.backgroundColor = UIColor.greenColor()
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ViewController2.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController2.swift
+//  TwoEntryDemo
+//
+//  Created by 鹏 朱 on 15/12/14.
+//  Copyright © 2015年 鹏 朱. All rights reserved.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.redColor()
+        
+        let button = UIButton()
+        button.addTarget(<#T##target: AnyObject?##AnyObject?#>, action: <#T##Selector#>, forControlEvents: <#T##UIControlEvents#>)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
